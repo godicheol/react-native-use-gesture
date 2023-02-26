4bcb666320207f8ce68f834c7d4da1284848e25a
godicheol <godicheol@gmail.com>
2023-02-27 00:50:08 +0900

    init

--- index.js
+++ index.js
@@ -10,6 +10,9 @@ function getDegree(cx, cy, x, y) {
     }
     return rad*180/Math.PI;
 }
+function setDegree(deg) {
+    return ((deg % 360) + 360) % 360;
+}
 function useGesture() {
     const callbacks = {
         start: null,
@@ -21,7 +24,7 @@ function useGesture() {
         rotate: null,
     }
 
-    const PRESS_DELAY = 100;
+    const PRESS_DELAY = 128;
 
     let pressCount = 0;
     let pressTimer = null;
@@ -34,14 +37,13 @@ function useGesture() {
 
     let moveX = 0;
     let moveY = 0;
-    let velocityX = 0;
-    let velocityY = 0;
 
     function setPressTimer() {
         if (pressTimer) {
             clearInterval(pressTimer);
         }
         pressCount = 0;
+        returnPress();
         pressTimer = setInterval(returnPress, PRESS_DELAY);
     }
     function unsetPressTimer() {
@@ -67,7 +69,6 @@ function useGesture() {
         }
     }
     function returnTap() {
-        unsetPressTimer(); // cancel press event
         if (callbacks.tap) {
             callbacks.tap({
                 initialX: initialTouches[0].pageX,
@@ -76,16 +77,14 @@ function useGesture() {
                 currentY: currentTouches[0].pageY,
                 moveX: moveX,
                 moveY: moveY,
-                velocityX: velocityX,
-                velocityY: velocityY,
                 initialTime: initialTime,
                 currentTime: currentTime,
             });
         }
     }
     function returnPress() {
-        currentTime = new Date().getTime();
         if (callbacks.press) {
+            currentTime = new Date().getTime();
             callbacks.press({
                 initialX: initialTouches[0].pageX,
                 initialY: initialTouches[0].pageY,
@@ -93,20 +92,16 @@ function useGesture() {
                 currentY: currentTouches[0].pageY,
                 moveX: moveX,
                 moveY: moveY,
-                velocityX: velocityX,
-                velocityY: velocityY,
                 initialTime: initialTime,
                 currentTime: currentTime,
-                n: pressCount++,
+                count: pressCount++,
             });
         }
     }
     function returnSwipe() {
-        moveX = currentTouches[0].pageX-initialTouches[0].pageX;
-        moveY = currentTouches[0].pageY-initialTouches[0].pageY;
-        velocityX = Math.abs(moveX)/(currentTime-initialTime);
-        velocityY = Math.abs(moveY)/(currentTime-initialTime);
         if (callbacks.swipe) {
+            moveX = currentTouches[0].pageX-initialTouches[0].pageX;
+            moveY = currentTouches[0].pageY-initialTouches[0].pageY;
             callbacks.swipe({
                 initialX: initialTouches[0].pageX,
                 initialY: initialTouches[0].pageY,
@@ -114,49 +109,54 @@ function useGesture() {
                 currentY: currentTouches[0].pageY,
                 moveX: moveX,
                 moveY: moveY,
-                velocityX: velocityX,
-                velocityY: velocityY,
                 initialTime: initialTime,
                 currentTime: currentTime,
             });
         }
     }
     function returnPinch() {
-        const initialDistance = getDistance(
-            initialTouches[0].pageX,
-            initialTouches[0].pageY,
-            initialTouches[1].pageX,
-            initialTouches[1].pageY
-        );
-        const currentDistance = getDistance(
-            currentTouches[0].pageX,
-            currentTouches[0].pageY,
-            currentTouches[1].pageX,
-            currentTouches[1].pageY
-        );
         if (callbacks.pinch) {
+            moveX = currentTouches[0].pageX-initialTouches[0].pageX+currentTouches[1].pageX-initialTouches[1].pageX;
+            moveY = currentTouches[0].pageY-initialTouches[0].pageY+currentTouches[1].pageY-initialTouches[1].pageY;
+            const initialDistance = getDistance(
+                initialTouches[0].pageX,
+                initialTouches[0].pageY,
+                initialTouches[1].pageX,
+                initialTouches[1].pageY
+            );
+            const currentDistance = getDistance(
+                currentTouches[0].pageX,
+                currentTouches[0].pageY,
+                currentTouches[1].pageX,
+                currentTouches[1].pageY
+            );
             callbacks.pinch({
                 initialDistance: initialDistance,
                 currentDistance: currentDistance,
+                moveX: moveX,
+                moveY: moveY,
                 initialTime: initialTime,
                 currentTime: currentTime,
             });
         }
     }
     function returnRotate() {
-        const cx = (initialTouches[0].pageX+initialTouches[1].pageX)*0.5;
-        const cy = (initialTouches[0].pageY+initialTouches[1].pageY)*0.5;
-        const id1 = getDegree(cx, cy, initialTouches[0].pageX, initialTouches[0].pageY);
-        const id2 = getDegree(cx, cy, initialTouches[1].pageX, initialTouches[1].pageY);
-        const cd1 = getDegree(cx, cy, currentTouches[0].pageX, currentTouches[0].pageY);
-        const cd2 = getDegree(cx, cy, currentTouches[1].pageX, currentTouches[1].pageY);
-        const d1 = id1 - cd1;
-        const d2 = id2 - cd2;
         if (callbacks.rotate) {
+            const cx = (initialTouches[0].pageX+initialTouches[1].pageX)*0.5;
+            const cy = (initialTouches[0].pageY+initialTouches[1].pageY)*0.5;
+            const id1 = getDegree(cx, cy, initialTouches[0].pageX, initialTouches[0].pageY);
+            const id2 = getDegree(cx, cy, initialTouches[1].pageX, initialTouches[1].pageY);
+            const cd1 = getDegree(cx, cy, currentTouches[0].pageX, currentTouches[0].pageY);
+            const cd2 = getDegree(cx, cy, currentTouches[1].pageX, currentTouches[1].pageY);
+            const d1 = id1 - cd1;
+            const d2 = id2 - cd2;
+            const d = Math.max(d1, d2);
             callbacks.rotate({
-                degree: ((Math.max(d1, d2) % 360) + 360) % 360,
-                degree1: ((d1 % 360) + 360) % 360,
-                degree2: ((d2 % 360) + 360) % 360,
+                degree: setDegree(d),
+                degree1: setDegree(d1),
+                degree2: setDegree(d2),
+                moveX: moveX,
+                moveY: moveY,
                 initialTime: initialTime,
                 currentTime: currentTime,
             });
@@ -187,12 +187,9 @@ function useGesture() {
                 currentTouches = touches;
                 countTouches = touches.length;
                 countMaxTouches = touches.length;
-                initialTime = new Date().getTime();
-                currentTime = initialTime;
+                currentTime = new Date().getTime();
                 moveX = 0;
                 moveY = 0;
-                velocityX = 0;
-                velocityY = 0;
                 if (countTouches === 1) {
                     setPressTimer();
                 } else {
@@ -213,16 +210,16 @@ function useGesture() {
             },
             onPanResponderEnd: function(e, gestureState) {
                 const { touches } = e.nativeEvent;
+                unsetPressTimer(); // cancel press event
                 currentTime = new Date().getTime();
                 if (countMaxTouches === 1) {
                     returnTap();
                 }
-                if (countTouches === 1) {
-                    initialTime = currentTime;
-                }
                 initialTouches = touches;
                 currentTouches = touches;
                 countTouches = touches.length;
+                moveX = 0;
+                moveY = 0;
             },
             onPanResponderRelease: function(e, gestureState) {
                 currentTime = new Date().getTime();
